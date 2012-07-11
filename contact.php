0e3a07d4342b8db11a4a07b1d490c09eafe72059
mirzamehdiraza <mirzamehdiraza@gmail.comgit config --global user.email mirza.mehdiraza@gmail.com>
2012-07-11 06:24:53 -0700

    first commit

---
+++ contact.php
@@ -0,0 +1,247 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>pakpoliticians.com</title>
+<meta name="keywords" content="Politicians of Pakistan Pakistani Politician">
+</head>
+
+<body background="images/bg.jpg">
+
+<div align="center">
+
+<table border="0" cellpadding="0" cellspacing="0" width="1055" height="783">
+	<!-- MSTableType="layout" -->
+	<tr>
+		<td height="78" colspan="9" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentHead" -->
+		<p align="center"><font size="7" face="Arial Black" color="#FFFFFF">
+PakPoliticians.com</font></td>
+	</tr>
+	<tr>
+		<td valign="top" rowspan="5" bgcolor="#89C434">
+		<!-- MSCellType="NavBody" -->
+		<table border="1" width="100%" id="table5" height="454">
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;<a href="index.htm"><span style="text-decoration: none"><font color="#FFFFFF">Home</font></span></a></b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;About 
+		Site</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>
+		&nbsp;Pakistani Politicians</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>
+		&nbsp;Politicians Profiles</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;Latest 
+		News</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;Articles</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>
+		&nbsp;Politicians Forum</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>
+		&nbsp;Community</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;Books</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;Photos 
+		Gallery</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;History 
+		of Politics</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;History 
+		of Politicians</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><font color="#FFFFFF" face="Arial" size="2"><b>&nbsp;Feed 
+		Back</b></font></td>
+	</tr>
+	<tr>
+		<td align="left"><b><font face="Arial" size="2" color="#FFFFFF">&nbsp;</font></b><font color="#FFFFFF" face="Arial" size="2"><b>Contact 
+		us</b></font></td>
+	</tr>
+	  </table>		</td>
+		<td bgcolor="#89C434"></td>
+		<td bgcolor="#89C434"></td>
+		<td bgcolor="#89C434"></td>
+		<td bgcolor="#89C434"></td>
+		<td bgcolor="#89C434"></td>
+		<td bgcolor="#89C434"></td>
+		<td height="1" bgcolor="#89C434"></td>
+		<td rowspan="5" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="NavBody2" -->
+		<div style="position: absolute; width: 113px; height: 338px; z-index: 1; left: 1005px; top: 93px" id="layer1">
+	<img border="0" src="images/jinnah.jpg" width="130" height="169"><p align="center">
+	<font face="Arial" size="1">Quaid-e-Azam Mohammad Ali Jinnah, Founder of 
+	Pakistan. (1927 - 1948)</font></div>
+ </td>
+	</tr>
+	<tr>
+		<td bgcolor="#89C434">&nbsp;</td>
+		<td height="122" colspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentHead2" -->
+		&nbsp;
+		<p><table width="400" border="0" align="center" cellpadding="3" cellspacing="1">
+<tr>
+<td><strong>Contact Form </strong></td>
+</tr>
+</table>
+
+        <table width="400" border="0" align="center" cellpadding="0" cellspacing="1">
+<tr>
+<td><form name="form1" method="post" action="send_contact.php">
+<table width="95%" border="0" cellspacing="1" cellpadding="3">
+<tr>
+<td width="16%">Subject</td>
+<td width="2%">:</td>
+<td width="82%"><input name="subject" type="text" id="subject" size="50"></td>
+</tr>
+<tr>
+<td>Detail</td>
+<td>:</td>
+<td><textarea name="detail" cols="47" rows="4" id="detail"></textarea></td>
+</tr>
+<tr>
+<td>Name</td>
+<td>:</td>
+<td><input name="name" type="text" id="name" size="50"></td>
+</tr>
+<tr>
+<td>Email</td>
+<td>:</td>
+<td><input name="customer_mail" type="text" id="customer_mail" size="50"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
+</tr>
+</table>
+</form></td>
+</tr>
+</table>
+</p>	  </td>
+	</tr>
+	<tr>
+		<td bgcolor="#89C434">&nbsp;</td>
+		<td height="127" colspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentBody" -->
+		&nbsp;</td>
+	</tr>
+	<tr>
+		<td bgcolor="#89C434">&nbsp;</td>
+		<td height="130" colspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentBody2" -->
+		&nbsp;
+		<p align="center">&nbsp;</p>
+		<p align="center">&nbsp;</p>
+		<p align="center"><b><font face="Arial" color="#FFFFFF">
+	  <a href="index.htm"><font color="#FFFFFF">Home</font></a></font></b></td>
+	</tr>
+	<tr>
+		<td bgcolor="#89C434">&nbsp;</td>
+		<td height="127" colspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot" -->
+		&nbsp;</td>
+	</tr>
+	<tr>
+		<td width="158" rowspan="7" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</td>
+		<td valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</td>
+		<td height="27" colspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</td>
+		<td width="177" rowspan="7" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</td>
+	</tr>
+	<tr>
+		<td width="16" rowspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</td>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td colspan="4" align="center" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td height="33" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+	</tr>
+	<tr>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td align="left" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td align="left" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td height="24" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+	</tr>
+	<tr>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td align="left" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td align="left" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td height="23" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+	</tr>
+	<tr>
+		<td width="96" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td width="104" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td width="155" align="left" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td width="98" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+		<td width="155" align="left" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+		<td width="96" height="23" bgcolor="#89C434"><font color="#000000">&nbsp;</font></td>
+	</tr>
+	<tr>
+		<td height="48" colspan="6" valign="top" bgcolor="#89C434">
+		  <font color="#000000">
+		<!-- MSCellType="ContentFoot2" -->
+		&nbsp;</font></td>
+	</tr>
+	<tr>
+		<td height="20" colspan="6" valign="top" bgcolor="#89C434">
+		<!-- MSCellType="ContentFoot2" -->
+		<p align="center">Copyright © 2009&nbsp;&nbsp; pakpoliticians.com&nbsp;&nbsp; 
+All Rights Reserved</td>
+	</tr>
+</table>
+
+</div>
+
+</body>
+
+</html>
